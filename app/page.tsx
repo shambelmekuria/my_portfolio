5b9c2498c37d09f1f212b1a5ebe88aeabd438964
shambelmekuria <shambelmekuria2022@gmail.com>
2026-05-10 22:28:09 +0300

    portfolio, install react icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/hero";
 import AboutUs from "@/components/home/about";
 import Navbar from "@/components/nav";
+import Portfolio from "@/components/portfolio";
 import Section from "@/components/section";
 import { SectionHeader } from "@/components/section.header";
 import Image from "next/image";
@@ -10,8 +11,8 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Navbar />
       <Hero />
-      <AboutUs/>
-     
+      <AboutUs />
+      <Portfolio />
     </div>
   );
 }
